7411fbb097237e8ff8e4dcd82602c149ef63f5fe
Harsh Singh <kmrharsh50@gmail.com>
2021-11-18 16:03:26 +0530

    [Harsh]Added: Project Logger Features Implemented

---
+++ Logger-Project/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ProjectLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello !");
+            AddNumber add = new AddNumber();
+            add.Sum(2,3);
+        }
+    }
+}
